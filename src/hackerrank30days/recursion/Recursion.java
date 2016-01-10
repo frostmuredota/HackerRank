d335efd7a392b3f86078de88e3cad37fddd0da3a
frostmuredota <ramduran2011@gmail.com>
2016-01-10 16:27:32 -0300

    update again

--- src/hackerrank30days/recursion/Recursion.java
+++ src/hackerrank30days/recursion/Recursion.java
@@ -1,7 +1,21 @@
 package hackerrank30days.recursion;
 
+import java.util.Scanner;
+
 /**
  * Created by Arthas on 10-01-2016.
  */
 public class Recursion {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        //Take Input
+        int gcd=find_gcd(a,b);
+        System.out.println(gcd);
+    }
+    static  int find_gcd(int a,int b){
+        if (b==0) return a;
+        return find_gcd(b,a%b);
+    }
 }
