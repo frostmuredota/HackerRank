f09f24b4fb6653c42b79d8f49bdcde2e90e437dc
rduran <rduran@nisum.com>
2016-01-11 19:35:23 -0300

    finish

--- src/hackerrank30days/Arrays2D/Arrays2d.java
+++ src/hackerrank30days/Arrays2D/Arrays2d.java
@@ -2,6 +2,7 @@ package hackerrank30days.Arrays2D;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by ramon on 11-01-16.
@@ -9,6 +10,7 @@ import java.util.List;
 public class Arrays2d {
     static int suma_mayor = 0;
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int[][] matrix = new int[6][6];
         int [][] aux = new int [3][3];
 
@@ -18,24 +20,18 @@ public class Arrays2d {
             }
         }
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                //System.out.print("["+matrix[i][j]+""+"]");
-                System.out.print("[" + matrix[i][j] + "" + "]");
-            }
-            System.out.println();
-        }
 
-        System.out.println();
-
-         int point = 0;
 
         for (int i = 1; i <matrix.length-1 ; i++) {
             for (int j = 1; j <matrix.length-1 ; j++) {
-                System.out.println("("+i+","+j+")");
+                aux = submatrix(matrix,i,j);
+                if(suma(aux)>suma_mayor){
+                    suma_mayor=suma(aux);
+                }
             }
-            System.out.println();
         }
+
+        System.out.println("SUMA MAYOR: "+suma_mayor);
 
     }
 
@@ -49,12 +45,21 @@ public class Arrays2d {
         suma += -(matrix[matrix.length/2][(matrix.length/2)-1]+matrix[matrix.length/2][(matrix.length/2)+1]);
         return suma;
     }
-   /*public static [][]submatrix(int [][]matrix,int x,int y){
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
+   public static int[][]submatrix(int [][]matrix,int x,int y){
+        int [][] aux = new int[3][3];
+        int a = 0;
+        int b=0;
+       for (int i = x-1; i < x+2; i++) {
+           for (int j = y-1; j <y+2 ; j++) {
+               aux[a][b]=matrix[i][j];
+               b++;
+           }
+           b=0;
+           a++;
 
-            }
-        }
+       }
+
+        return aux;
     }
-*/
+
 }
