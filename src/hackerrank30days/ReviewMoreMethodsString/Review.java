570c0b686011e00937b6caaea0671fa5c3a8c2b5
rduran <rduran@nisum.com>
2016-01-20 11:26:28 -0300

    Day 20 Review Solved

--- src/hackerrank30days/ReviewMoreMethodsString/Review.java
+++ src/hackerrank30days/ReviewMoreMethodsString/Review.java
@@ -1,10 +1,21 @@
 package hackerrank30days.ReviewMoreMethodsString;
 
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 /**
  * Created by ramon on 19-01-16.
  */
 public class Review {
     public static void main(String[] args) {
+        String pattern = "[ !,?\\._'@]+";
+        Scanner sc = new Scanner(System.in);
+        String phrase = sc.nextLine();
+        StringTokenizer stk = new StringTokenizer(phrase,pattern);
+        System.out.println(stk.countTokens());
+        while (stk.hasMoreTokens()){
+            System.out.println(stk.nextToken());
+        }
 
     }
 }
