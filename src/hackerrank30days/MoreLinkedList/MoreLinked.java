51524550c1999eef9c5ab716a415fcd0890ce640
frostmuredota <ramduran2011@gmail.com>
2016-01-29 16:45:53 -0300

    Remove Duplicates Nodes solved

--- src/hackerrank30days/MoreLinkedList/MoreLinked.java
+++ src/hackerrank30days/MoreLinkedList/MoreLinked.java
@@ -41,35 +41,27 @@ public class MoreLinked {
             int ele=sc.nextInt();
             head=insert(head,ele);
         }
-        head=removeDuplicates(head);
+        head=removeDuplicates(head);;
         display(head);
-
     }
 
     private static Node removeDuplicates(Node head) {
         Node start=head;
         Node aux = null;
+        HashMap<Node,Integer> map = new HashMap<Node, Integer>();
         List<Integer> l = new ArrayList<Integer>();
-        Set<Integer> hs = new HashSet<Integer>();
         while(start!=null) {
-            if(start.next==null){
-                l.add(start.data);
-                break;
+            if(map.containsValue(start.data)){
+                start=start.next;
             }else{
-                if(start.data==start.next.data){
-                    l.add(start.data);
-                    start=start.next.next;
-                }else{
-                    l.add(start.data);
-                    start=start.next;
-                }
+                map.put(start,start.data);
+                l.add(start.data);
+                start=start.next;
             }
         }
-        hs.addAll(l);
-        l.clear();
-        l.addAll(hs);
-        for (int i = 0; i <l.size() ; i++) {
-            aux=insert(aux,l.get(i));
+        for (Integer integer : l) {
+
+            aux = insert(aux,integer);
         }
         return aux;
     }
